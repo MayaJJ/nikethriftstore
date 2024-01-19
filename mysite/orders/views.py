2a91178111f872f160aab25ad9ed276a875560cd
MayaJJ <mariamemeliejobe@gmail.com>
2024-01-19 16:08:47 +0500

    Created orders app, to get products in orders, orders have a order and orderitem, containing product as foreign key and user, also added the urls for it and views to add items in cards and home view

---
+++ mysite/orders/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, redirect
+from .models import Order, OrderItem
+from carts.models import ShoppingCart, CartItem
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.shortcuts import render, redirect, get_object_or_404
+# Create your views here.
+
+
+def add_order(request, shopping_cart):
+    shopping_cart = ShoppingCart.objects.get(pk=shopping_cart)
+    user = shopping_cart.user
+    order = Order(user=shopping_cart.user, price=shopping_cart.price)
+    order.save()
+    cart_item = CartItem.objects.filter(shopping_cart=shopping_cart)
+    for item in cart_item:
+        OrderItem(order=order, product=item.product).save()
+        item.product.is_sold = True
+        item.product.save()
+    shopping_cart.delete()
+    messages.success(request, 'You successfully created an order!')
+    return redirect('orders:home', user.username)
+
+
+def home(request, username):
+    user = get_object_or_404(User, username=username)
+    orders = Order.objects.filter(user=user)
+    data = []
+
+    for order in orders:
+        items = OrderItem.objects.filter(order=order).select_related('product')
+        temp = [item.product for item in items]
+        data.append({'order_id': order.id, 'price': order.price, 'items': temp})
+
+    return render(request, 'orders/home.html', {'user': user, 'orders': data})
