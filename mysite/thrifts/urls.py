9bfcb1c780bef5e18c8e94d5506bf9a475bcda0b
MayaJJ <mariamemeliejobe@gmail.com>
2024-01-19 15:55:29 +0500

    Added Urls for the views i wrote for products/thrifts

---
+++ mysite/thrifts/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, re_path
+from . import views
+
+app_name = 'thrifts'
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('home/', views.home, name='home'),
+    path('product/', views.list, name='list'),
+    path('product/<int:product_id>/', views.detail, name='detail'),
+    path('product/<int:product_id>/edit', views.edit, name='edit'),
+    path('product/<int:product_id>/delete', views.delete, name='delete'),
+    path('sell/', views.sell, name='sell'),
+    path('seller/<seller>/', views.seller, name="seller"),
+]
